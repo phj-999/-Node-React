767bbe9e8134d0ebf40f13d12cf8171b9c6e26b8
phj-999 <865779043@qq.com>
2021-11-19 10:22:12 +0800

    update

--- music/src/pages/discover/c-pages/album/index.js
+++
@@ -1,9 +0,0 @@
-import React, { memo } from 'react'
-
-export default memo(function HYAlbum() {
-    return (
-        <div>
-            HYAlbum
-        </div>
-    )
-})
--- r-demo/mobile/src/setupTests.js
+++
@@ -1,2 +0,0 @@
-List.isList()判断是不是List类型
-List.of()创建一个list数组
