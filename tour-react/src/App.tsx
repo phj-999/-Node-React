1e969edaa9f6e6768cc66d487207a35ae08fc9e3
phj-999 <865779043@qq.com>
2021-12-03 21:53:40 +0800

    样式布局修改

--- tour-react/src/App.tsx
+++ tour-react/src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Typography } from "antd";
 
-import { Header, Footer, Carousel, SideMenu } from "./components";
+import { Header, Footer, Carousel, SideMenu, ProductCollection, BusinessPartners } from "./components";
 import styles from "./App.module.css";
 import { productList1, productList2, productList3 } from "./mockups";
+
+
+import sideImage from './assets/images/sider_2019_12-09.png';
+import sideImage2 from './assets/images/sider_2019_02-04.png';
+import sideImage3 from './assets/images/sider_2019_02-04-2.png';
 
 function App() {
   return (
@@ -48,6 +53,7 @@ function App() {
           sideImage={sideImage3}
           products={productList3}
         />
+        <BusinessPartners />
       </div>
       <Footer />
     </div>
--- tour-react/src/components/productCollection/ProductCollection.tsx
+++ tour-react/src/components/productCollection/ProductCollection.tsx
@@ -1,40 +1,128 @@
 import React from "react";
 import styles from "./ProductCollection.module.css";
-import { Row, Col, Typography, Divider } from "antd";
+import { Row, Col, Divider } from "antd";
+import { ProductImage } from "./ProductImage";
 
 interface PropsType {
-    title : JSX.Element,//title可以接受传入一个react组件，所以类型是JSX.Element
-    sideImage: string,
-    products: any[]  //有不确定定型
+  title: JSX.Element; //title可以接受传入一个react组件，所以类型是JSX.Element
+  sideImage: string;
+  products: any[]; //有不确定定型
 }
 
-export const ProductCollection: React.FC<PropsType> = ({title,sideImage,products})=>
-{
+export const ProductCollection: React.FC<PropsType> = ({
+  title,
+  sideImage,
+  products,
+}) => {
+  return (
     <div className={styles.content}>
-         <Divider orientation="left">{title}</Divider>
-          {/**栅格布局 整体分为两个区块 一个span是4 一个是20 */}
-          <Col span={4}>
+      <Divider orientation="left">{title}</Divider>
+      <Row>
+        {/**栅格布局 整体分为两个区块 一个span是4 一个是20 */}
+        <Col span={4}>
           <img src={sideImage} className={styles["side-image"]} alt="" />
-          </Col>
-          {/**此处为右侧栅格，整体分为上下两个部分 2个行布局Row */}
-          <Col span={20}>
-              {/**此处为上面的行 又分左右两部分 */}
+        </Col>
+        {/**此处为右侧栅格，整体分为上下两个部分 2个行布局Row */}
+        <Col span={20}>
+          {/**此处为上面的行 又分左右两部分 */}
+          <Row>
+            {/**上方左侧 放推荐页最大图面 */}
+            <Col span={12}>
+              <ProductImage
+                id={products[0].id}
+                size={"large"}
+                title={products[0].title}
+                imageSrc={products[0].touristRoutePictures[0].url}
+                price={products[0].price}
+              />
+            </Col>
+            {/**上方右侧 放4个图片 又分上下两行 */}
+            <Col span={12}>
+              {/**各分两列 */}
               <Row>
-                  {/**上方左侧 放推荐页最大图面 */}
-                  <Col span={12}>
-                  sxx
-                  </Col>
-                  {/**上方右侧 放4个图片 又分上下两行 */}
-                  <Col span={12}>
-                      {/**各分两列 */}
-                  <Row></Row>
-                  <Row></Row>
-                  </Col>
+                <Col span={12}>
+                  <ProductImage
+                    id={products[1].id}
+                    size="small"
+                    title={products[1].title}
+                    imageSrc={products[1].touristRoutePictures[0].url}
+                    price={products[1].price}
+                  />
+                </Col>
+                <Col span={12}>
+                  <ProductImage
+                    id={products[2].id}
+                    size="small"
+                    title={products[2].title}
+                    imageSrc={products[2].touristRoutePictures[0].url}
+                    price={products[2].price}
+                  />
+                </Col>
               </Row>
+              <Row>
+                <Col span={12}>
+                  <ProductImage
+                    id={products[3].id}
+                    size="small"
+                    title={products[3].title}
+                    imageSrc={products[3].touristRoutePictures[0].url}
+                    price={products[3].price}
+                  />
+                </Col>
+                <Col span={12}>
+                  <ProductImage
+                    id={products[4].id}
+                    size="small"
+                    title={products[4].title}
+                    imageSrc={products[4].touristRoutePictures[0].url}
+                    price={products[4].price}
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
 
-{/**此处为下面的行 */}
-              <Row>
-              </Row>
-          </Col>
+          {/**此处为下面的行 */}
+          <Row>
+            <Col span={6}>
+              <ProductImage
+                id={products[5].id}
+                size="small"
+                title={products[5].title}
+                imageSrc={products[5].touristRoutePictures[0].url}
+                price={products[5].price}
+              />
+            </Col>
+            <Col span={6}>
+              <ProductImage
+                id={products[6].id}
+                size="small"
+                title={products[6].title}
+                imageSrc={products[6].touristRoutePictures[0].url}
+                price={products[6].price}
+              />
+            </Col>
+            <Col span={6}>
+              <ProductImage
+                id={products[7].id}
+                size="small"
+                title={products[7].title}
+                imageSrc={products[7].touristRoutePictures[0].url}
+                price={products[7].price}
+              />
+            </Col>
+            <Col span={6}>
+              <ProductImage
+                id={products[8].id}
+                size="small"
+                title={products[8].title}
+                imageSrc={products[8].touristRoutePictures[0].url}
+                price={products[8].price}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </div>
-}
+  );
+};
